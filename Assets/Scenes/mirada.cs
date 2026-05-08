72753670a566f18ad5e606734347da4f4a7bb9b5
marcmoncho <marc.moncho@estudiantat.upc.edu>
2026-05-08 20:16:36 +0200

    Nuevos Sonidos
    - Disparar portal 1
    - Disparar portal 2
    - Tirar Pelota
    - Pasos (a mejorar)

--- Assets/Scenes/mirada.cs
+++ Assets/Scenes/mirada.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.InputSystem.Controls;
 using System.Collections;
+
 public class mirada : MonoBehaviour
 {
     public GameObject Robert;
@@ -26,6 +27,12 @@ public class mirada : MonoBehaviour
 
     public float SepararX;
     public Animator Mira;
+
+    [Header("Sonidos de FMOD")]
+    public FMODUnity.EventReference sonidoPortalAzul;   // Clic Izquierdo
+    public FMODUnity.EventReference sonidoPortalNaranja; // Clic Derecho
+    public FMODUnity.EventReference sonidoAgarre;        // Tecla E
+
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
@@ -33,7 +40,6 @@ public class mirada : MonoBehaviour
         rb = GetComponent<Rigidbody>();
         Ocupado = false;
     }
-
 
     void Update()
     {
@@ -46,89 +52,86 @@ public class mirada : MonoBehaviour
         transform.localRotation = Quaternion.Euler(girox, 0, 0);
         Robert.transform.Rotate(Vector3.up * x);
 
-
         Debug.DrawRay(transform.position, transform.forward * rayDistance, Color.red);
 
         RaycastHit hit;
-
         bool detectar = false;
 
         if (Physics.Raycast(transform.position, transform.forward, out hit, rayDistance, Objeto))
         {
-            // ¿Tiene Rigidbody O tiene el script de Gravedad?
-
             if (hit.transform.GetComponent<Rigidbody>() != null || hit.transform.GetComponent<CambioGravedad>() != null)
             {
                 detectar = true;
             }
-
         }
-            Mira.SetBool("Est", detectar);
+
+        Mira.SetBool("Est", detectar);
+
+        // --- AGARRAR OBJETOS (TECLA E) ---
         if (Physics.Raycast(transform.position, transform.forward, out hit, rayDistance, Objeto) && Input.GetKeyDown(KeyCode.E))
         {
             if (!Ocupado)
             {
-                Debug.Log(hit.transform.name);
                 rbExt = hit.transform.GetComponent<Rigidbody>();
                 fj.connectedBody = rbExt;
                 Ocupado = true;
+                // Sonido al agarrar
+                FMODUnity.RuntimeManager.PlayOneShot(sonidoAgarre, transform.position);
             }
-
             else
             {
-                Debug.Log("soltando");
-                fj.connectedBody = null; //Lo desenganchamos, pero luego hay que resetear las fisicas del objeto
+                fj.connectedBody = null;
                 rbExt.linearVelocity = Vector3.zero;
                 rbExt.angularVelocity = Vector3.zero;
                 Ocupado = false;
+                // Sonido al soltar (puedes usar el mismo o uno diferente)
+                FMODUnity.RuntimeManager.PlayOneShot(sonidoAgarre, transform.position);
             }
-
         }
 
+        // --- INVERTIR GRAVEDAD (TECLA Z) ---
         if (Physics.Raycast(transform.position, transform.forward, out hit, rayDistance) && Input.GetKeyDown(KeyCode.Z))
         {
-            Debug.Log("Invirtiendo");
             Gravedad = hit.transform.GetComponent<CambioGravedad>();
-
-            if (Gravedad.GetValor() < 0)
+            if (Gravedad != null)
             {
-                Gravedad.Alterar(9.8f);
-                Gravedad.Invertir = false;
+                if (Gravedad.GetValor() < 0)
+                {
+                    Gravedad.Alterar(9.8f);
+                    Gravedad.Invertir = false;
+                }
+                else
+                {
+                    Gravedad.Alterar(-9.8f);
+                }
+                Gravedad.StartCoroutine(Gravedad.EfectoInversion());
             }
-
-            else
-            {
-                Gravedad.Alterar(-9.8f);
-            }
-
-            Gravedad.StartCoroutine(Gravedad.EfectoInversion());
         }
 
-
+        // --- PORTAL NARANJA (CLIC DERECHO) ---
         RaycastHit hitPared;
         if (Physics.Raycast(transform.position, transform.forward, out hitPared, DistanciaPortal, MuroPortal) && Input.GetMouseButtonDown(1))
         {
-            //Vector3 Separar = new Vector3(SepararX, 0f, 0f);
-
             PortalA.transform.position = hitPared.point + (hitPared.normal * SepararX);
-            //PortalA.transform.rotation = Quaternion.FromToRotation(PortalA.transform.forward, hitPared.normal) * PortalA.transform.rotation;
             PortalA.transform.rotation = Quaternion.LookRotation(hitPared.normal);
-            Debug.Log("Portal");
+
+            // REPRODUCIR SONIDO
+            FMODUnity.RuntimeManager.PlayOneShot(sonidoPortalNaranja, transform.position);
+            Debug.Log("Portal Naranja");
         }
 
+        // --- PORTAL AZUL (CLIC IZQUIERDO) ---
         RaycastHit hitPared2;
-
         if (Physics.Raycast(transform.position, transform.forward, out hitPared2, DistanciaPortal, MuroPortal) && Input.GetMouseButtonDown(0))
         {
-            //Vector3 Separar = new Vector3(SepararX, 0f, 0f);
-
             PortalB.transform.position = hitPared2.point + (hitPared2.normal * SepararX);
-            //PortalB.transform.rotation = Quaternion.FromToRotation(PortalB.transform.forward, hitPared2.normal) * PortalB.transform.rotation;
             PortalB.transform.rotation = Quaternion.LookRotation(hitPared2.normal);
-            Debug.Log("Portal");
+
+            // REPRODUCIR SONIDO
+            FMODUnity.RuntimeManager.PlayOneShot(sonidoPortalAzul, transform.position);
+            Debug.Log("Portal Azul");
         }
+
         Debug.DrawRay(transform.position, transform.forward * DistanciaPortal, Color.blue);
-
     }
-
 }
